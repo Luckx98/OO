95cc784255b565234897eabb4463b08ac46f5df8
Luckx98 <lucasmacedob@gmail.com>
2021-07-30 21:38:27 -0300

    Fazendo algumas pequenas mudanças de identação e organização

--- Trabalhos/src/TP01.java
+++ Trabalhos/src/TP01.java
@@ -4,9 +4,10 @@ import java.util.*;;
 public class TP01 {
 
 	static class Clientes {
-		public String nome;
-		public String endereco;
-		public String telefone;
+		private String nome;
+		private String endereco;
+		private long telefone;
+		
 	}
 
 	private static class Produtos {
@@ -23,15 +24,14 @@ public class TP01 {
 		
 		// Dados do clientes
 		Scanner cliente = new Scanner(System.in);
-		Clientes client;
-		client = new Clientes();
+		Clientes client = new Clientes();
 
 		// Dados do produto
 		Scanner produto = new Scanner(System.in);
-		Produtos produt;
-		produt = new Produtos();
+		Produtos produt = new Produtos();
 
 		Scanner menu = new Scanner(System.in);
+
 
 		do {
 			System.out.print("##--Controle de clientes e produtos--##\n\n");
@@ -52,23 +52,25 @@ public class TP01 {
 				menu.close();
 				break;
 			}
+			
+
 
 			switch (opcao) {
 			case 1: {
-				System.out.println("Insira o nome: ");
+				System.out.println("Insira o nome:");
 				client.nome = cliente.nextLine();
-				System.out.println("Insira o endereço: ");
+				client.nome = client.nome.trim();
+				System.out.println("Insira o endereço:");
 				client.endereco = cliente.nextLine();
-				System.out.println("Insira o telefone: ");
-				client.telefone = cliente.next();
+				System.out.println("Insira o telefone:");
+				client.telefone = cliente.nextLong();
 				System.out.print("\n");
-
 				break;
 			}
 			case 2: {
 				System.out.println("Por favor, insira o nome para verificar o cadastro: ");
 				client.nome = cliente.nextLine();
-//				if (client.nome) {
+//				if (client.nome == "Lucas") {
 //					System.out.println("Nome:" + client.nome);
 //					System.out.println("Endereçoi:" + client.endereco);
 //					System.out.println("Telefone: " + client.telefone);
@@ -81,13 +83,13 @@ public class TP01 {
 				System.out.println("Defina a quantidade de produtos a serem cadastrados: ");
 				produt.qntProdutos = produto.nextInt();
 				for (int i=0; i < produt.qntProdutos; i++) {
-					System.out.println("Insira o nome do produto: ");
+					System.out.println("Insira o nome do produto:");
 					produt.nomeProduto = produto.nextLine();
-					System.out.println("Insira a descrição do produto: ");
+					System.out.println("Insira a descrição do produto:");
 					produt.descProduto = produto.nextLine();
-					System.out.println("Insira o valor de compra do produto: ");
+					System.out.println("Insira o valor de compra do produto:");
 					produt.valorProduto = produto.nextInt();
-					System.out.println("Insira a porcentagem de lucro do produto: " + "%");
+					System.out.println("Insira a porcentagem de lucro do produto: ");
 					produt.porcentProduto = produto.nextInt();
 					System.out.println("Insira a quantidade em estoque do produto: ");
 					produt.qntEstoque = produto.nextInt();
@@ -96,6 +98,9 @@ public class TP01 {
 				}
 				break;
 			}
+			default:
+                System.out.println("Opção digitada é inválida!!!\n\n");
+                break;
 			}
 		} while (opcao != 7);
 
