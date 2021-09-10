1832dbd3259581b452d828cf560b94a0701c2531
Samuel Avila <samuel2001.furtadoavila@gmail.com>
2021-09-10 16:36:58 -0300

    Addicionando classe bilheteria

---
+++ Trabalhos/src/negocio/Bilheteria.java
@@ -0,0 +1,39 @@
+package negocio;
+
+public class Bilheteria {
+    private Sessao[] sessao;
+    private Cliente cliente;
+    private Funcionario funcionario;
+  
+    public Bilheteria(Sessao[] s, Cliente c, Funcionario f){
+        sessao = s;
+        cliente = c;
+        funcionario = f;
+        }
+
+    public Sessao[] getSessao() {
+        return sessao;
+    }
+        
+    public void setSessao(Sessao[] sessao) {
+        this.sessao = sessao;
+    }
+        
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+        
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+     }
+        
+    public Cliente getCliente() {
+        return cliente;
+    }
+        
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+
+}
