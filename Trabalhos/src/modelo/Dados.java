8aea0af1d6c4512149442679ade49f51280f4b62
Luckx98 <lucasmacedob@gmail.com>
2021-10-01 16:45:50 -0300

    Criando a Classe de Dados

---
+++ Trabalhos/src/modelo/Dados.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import java.util.*;
+
+public class Dados {
+    private Cliente[] clientes = new Cliente[30];
+    private int qntClientes = 0;
+    private Filme[] filmes = new Filme[30];
+    private int qntFilmes = 0;
+    private Funcionario[] funcionarios = new Funcionario[10];
+    private int qntFuncionarios = 0;
+    private Ingresso[] ingressos = new Ingresso[30];
+    private int qntIngressos = 0;
+}
